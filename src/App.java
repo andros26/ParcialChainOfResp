1cd4367771d19e5bf523b66b5ebb39c51a4c8ad7
andros26 <108378283+andros26@users.noreply.github.com>
2025-03-05 21:27:08 -0500

    Actividad finalizada

--- src/App.java
+++ src/App.java
@@ -4,21 +4,26 @@ import Servicios.CHandle20k;
 import Servicios.CHandle50k;
 import Servicios.CHandle5k;
 import Servicios.IHandlerBilletes;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner leer = new Scanner(System.in);
         IHandlerBilletes h1 = new CHandle100k();
         IHandlerBilletes h2 = new CHandle50k();
         IHandlerBilletes h3 = new CHandle20k();
         IHandlerBilletes h4 = new CHandle10k();
         IHandlerBilletes h5 = new CHandle5k();
 
+        System.out.println("Ingrese el monto a retirar(múltiplo de 5000):");
+        int monto =leer.nextInt();
+        leer.close();
         h1.setNext(h2);
         h2.setNext(h3);
         h3.setNext(h4);
         h4.setNext(h5);
 
 
-         h1.solicitudBilletes(645000);  // Cajero: Ingresa cantidad
+         h1.solicitudBilletes(monto);  // Cajero: Ingresa cantidad
     }
 }
